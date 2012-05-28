4cac820915e43ad2d8b623f6d4bf597684861910
Phu Huynh <phuhuynh333@gmail.com>
2012-05-28 10:54:34 -0700

    Implemented function to convert hex string to datastream, wrote test case for said funciton. Removed precompiled JSON library

--- src/unifying_functions.c
+++ src/unifying_functions.c
@@ -1,16 +1,18 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "unifying_functions.h"
 
-static char* data_to_hex_string(const uint8_t* data, size_t length); 
+static char* data_to_hex_string(const uint8_t* data, size_t size); 
+static uint8_t* hex_string_to_data(const char* hexstr, size_t length);
 
-char* convert_raw_to_unified(const uint8_t* data, size_t offset, size_t length,
+char* convert_raw_to_unified(const uint8_t* data, size_t offset, size_t size,
 	const char* type, const char* desc, const char* sensor) {
 	/* remove the sensor headers */
 	uint8_t* plain_data = data + offset; 
 	
 	/* convert datasteam into hex string */
-	char* hex_data = data_to_hex_string(plain_data, length-offset);
+	char* hex_data = data_to_hex_string(plain_data, size-offset);
 	
 	/* create a root cJSON object */		
 	cJSON* unified_root = cJSON_CreateObject();
@@ -18,8 +20,9 @@ char* convert_raw_to_unified(const uint8_t* data, size_t offset, size_t length,
 	/* add all elements */
 	cJSON_AddStringToObject(unified_root, "type", type);
 	cJSON_AddStringToObject(unified_root, "description", desc);
-	//Note max size of size_t varies per machine, meaning max length can vary 
-	cJSON_AddNumberToObject(unified_root, "length", length); 
+	/*Note max size of size_t varies per machine, 
+		meaning max size can vary */ 
+	cJSON_AddNumberToObject(unified_root, "size", size); 
 	cJSON_AddStringToObject(unified_root, "sensor", sensor);
 	int checksum = 0xdeadbeef; 
 	cJSON_AddNumberToObject(unified_root, "checksum", checksum);
@@ -39,14 +42,88 @@ uint8_t* convert_unified_to_raw (const char* data) {
 }
 
 
-static char* data_to_hex_string(const uint8_t* data, size_t length) {
-	char* hex_string = (char*) malloc(2*sizeof(char)*length+1);
+static char* data_to_hex_string(const uint8_t* data, size_t size) {
+	char* hex_string = (char*) malloc(2*sizeof(char)*size+1);
 	size_t i;
 	/* print data one byte at a time as a set of two hex chars */
-	for(i = 0; i < length; i++) {
+	for(i = 0; i < size; i++) {
 		sprintf(hex_string+(2*i),"%02x", data[i]);
 	}	
 	/* null byte */
-	hex_string[2*length] = '\0';
+	hex_string[2*size] = '\0';
 	return hex_string;
 } 
+
+static uint8_t* hex_string_to_data(const char* hexstr, size_t length) {
+	/*Note we expect length to be even given that the hex string
+	is twice as long as the bytestream */
+	uint8_t* bytestream = (uint8_t*) malloc(length/2*sizeof(uint8_t));
+	uint8_t* byteptr = bytestream;
+	uint8_t val;
+	char c;
+	size_t i;
+	for(i = 0; i < length; i++) {
+		int odd = (i & 1);
+		val = odd ? (val << 4) : 0;	
+		c = tolower(hexstr[i]);
+		switch(c) {
+			case '0':
+				val |= 0x0;
+				break;
+			case '1':
+				val |= 0x1;
+				break;
+			case '2':
+				val |= 0x2;
+				break;
+			case '3':
+				val |= 0x3;
+				break;
+			case '4':
+				val |= 0x4;
+				break;
+			case '5':
+				val |= 0x5;
+				break;
+			case '6':
+				val |= 0x6;
+				break;
+			case '7':
+				val |= 0x7;
+				break;
+			case '8':
+				val |= 0x8;
+				break;
+			case '9':
+				val |= 0x9;
+				break;
+			case 'a':
+				val |= 0xa;
+				break;
+			case 'b':
+				val |= 0xb;
+				break;
+			case 'c':
+				val |= 0xc;
+				break;
+			case 'd':
+				val |= 0xd;
+				break;
+			case 'e':
+				val |= 0xe;
+				break;
+			case 'f':
+				val |= 0xf;
+				break;
+			/* did not find a hex letter */
+			default: 
+				free(bytestream);
+				return NULL;
+		}		
+		if (odd) {
+			*byteptr = val;		
+			byteptr++;
+		}
+	}
+	return bytestream;
+}
--- test/translatetest.c
+++ test/translatetest.c
@@ -1,26 +1,142 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 
+/** 
+ * An equals check for two byte arrays 
+ * Returns: 0 on failure, 1 on success;
+ */
+int bytes_equal (const uint8_t* a1, const uint8_t* a2, size_t n) {
+	size_t i;
+	for (i = 0; i < n; i++) {
+		if (a1[i] != a2[i])
+			return 0;
+	}
+	return 1;
+}
+
 /* included here for unit tests since its a static function */
-static char* data_to_hex_string(const uint8_t* data, size_t length) {
-        char* hex_string = (char*) malloc(2*sizeof(char)*length+1);
+static char* data_to_hex_string(const uint8_t* data, size_t size) {
+        char* hex_string = (char*) malloc(2*sizeof(char)*size+1);
         size_t i;
         /* print data one byte at a time as a set of two hex chars */
-        for(i = 0; i < length; i++) {
+        for(i = 0; i < size; i++) {
                 sprintf(hex_string+(2*i),"%02x", data[i]);
         }       
         /* null byte */
-        hex_string[2*length] = '\0'; 
+        hex_string[2*size] = '\0'; 
         return hex_string;
 } 
 
+static int test_data_to_hex(const uint8_t* dummy_data, size_t size, 
+		const char* expected) {
+	char* dummy_str = data_to_hex_string(dummy_data, size);
+	if (strcmp(dummy_str, expected) != 0) {
+		printf("Expected and str do not match in test_data_to_hex. \n");
+		return -1;
+	}
+	free(dummy_str);
+	return 0;
+}
+
+/* included here for unit test since static */ 
+static uint8_t* hex_string_to_data(const char* hexstr, size_t length) {
+	/*Note we expect length to be even given that the hex string
+	is twice as long as the bytestream */
+	uint8_t* bytestream = (uint8_t*) malloc(length/2*sizeof(uint8_t));
+	uint8_t* byteptr = bytestream;
+	uint8_t val;
+	char c;
+	size_t i;
+	for(i = 0; i < length; i++) {
+		int odd = (i & 1);
+		val = odd ? (val << 4) : 0;	
+		c = tolower(hexstr[i]);
+		switch(c) {
+			case '0':
+				val |= 0x0;
+				break;
+			case '1':
+				val |= 0x1;
+				break;
+			case '2':
+				val |= 0x2;
+				break;
+			case '3':
+				val |= 0x3;
+				break;
+			case '4':
+				val |= 0x4;
+				break;
+			case '5':
+				val |= 0x5;
+				break;
+			case '6':
+				val |= 0x6;
+				break;
+			case '7':
+				val |= 0x7;
+				break;
+			case '8':
+				val |= 0x8;
+				break;
+			case '9':
+				val |= 0x9;
+				break;
+			case 'a':
+				val |= 0xa;
+				break;
+			case 'b':
+				val |= 0xb;
+				break;
+			case 'c':
+				val |= 0xc;
+				break;
+			case 'd':
+				val |= 0xd;
+				break;
+			case 'e':
+				val |= 0xe;
+				break;
+			case 'f':
+				val |= 0xf;
+				break;
+			/* did not find a hex letter */
+			default: 
+				free(bytestream);
+				return NULL;
+		}		
+		if (odd) {
+			*byteptr = val;		
+			printf("%u\n", *byteptr);
+			byteptr++;
+		}
+	}
+	return bytestream;
+}
+
+static int test_hex_to_data (const char* hexstr, size_t length, 
+		const uint8_t* expected) {
+	uint8_t* bytestream = hex_string_to_data(hexstr, length);	
+	if (!bytes_equal(expected, bytestream, length/2)) {
+		printf("Expected and data do not match in test_hex_to_data\n");	
+		return -1;
+	}
+	return 0;
+}
+
+
+
 
 int main() {
+	size_t size = 10;
+	size_t length = 20; 
 	uint8_t dummy_data[10] = {0, 12, 13, 14, 25, 26, 27, 33, 200, 255};
-	char* dummy_str = data_to_hex_string(dummy_data, 10);
-	printf(dummy_str);
-	printf("\n");
-	free(dummy_str);
+	char* hexstr = "000c0d0e191a1b21c8ff";
+	int retval = test_data_to_hex(dummy_data, 10, hexstr); 
+	printf("Return value for test_data_to_hex: %d\n", retval);
+	retval = test_hex_to_data(hexstr, length, dummy_data); 
+	printf("Return value for test_hex_to_data: %d\n", retval);
 	return 0; 			
 }
